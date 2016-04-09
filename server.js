ebf834b9fb1fb7633efcca2f74d7d2849c25a707
Slevin <nicolas.sadin@etixgroup.com>
2016-04-09 15:41:31 +0200

    Add APIs translation to get realtime datas, log

--- server.js
+++ server.js
@@ -34,33 +34,83 @@ app.get('/bus', function (req, res) {
 app.get('/bus/:id', function (req, res) {
     let id = req.params.id;
     request('http://opendata.vdl.lu/odaweb/index.jsp?cat=' + id).then((data) => {
-        res.send(formatBusLineDataForGoogleAPI(JSON.parse(data)));
-    });
-});
-
-app.get('/bus/:id/maths', function (req, res) {
-    let id = req.params.id;
-    request('http://opendata.vdl.lu/odaweb/index.jsp?cat=' + id).then((data) => {
-        data = formatBusLineDataForExerciseGeneration(JSON.parse(data));
+        data = formatBusLineDataForGoogleAPI(JSON.parse(data));
         res.send(data);
     });
 });
 
-function formatBusLineDataForExerciseGeneration(data) {
-    let ret = {paths: [], stops: []};
-    let id = 0;
-    for (let line of data.features) {
-        if (line.geometry.type == 'LineString') {
-            ret.paths.push(convertCoordFromEPSG2169ToNormal(line.geometry.coordinates));
-        } else {
-            let point = {
-                id: id++,
-                coord: proj4(luxEPSG2169).inverse(line.geometry.coordinates),
+app.get('/bus/:id/from/:startId/to/:stopId/maths', function (req, res) {
+    let id = req.params.id;
+    let startId = req.params.startId;
+    let stopId = req.params.stopId;
+    console.log('Requesting for stops/paths information for bus ' + id + '...');
+    request('http://opendata.vdl.lu/odaweb/index.jsp?cat=' + id).then((data) => {
+        console.log('Formatting Bus Line Data for Google API...');
+        let formattedData = formatBusLineDataForGoogleAPI(JSON.parse(data));
+        console.log('Getting coords for start number ' + startId + ' and stop number ' + stopId + '...');
+        let stops = getStopsCoordsByIds(formattedData, [startId, stopId]);
+        Promise.all([
+            getStopApiIdByCoord(res, stops[0]),
+            getStopApiIdByCoord(res, stops[1])
+        ]).then(function (stopApiIds) {
+            return Promise.all([
+                getStopScheduleByApiId(stopApiIds[0]),
+                getStopScheduleByApiId(stopApiIds[1])
+            ]);
+        }).then(function (stopSchedules) {
+            res.send(stopSchedules);
+        });
+    });
+});
+
+function getStopScheduleByApiId(stopApiId) {
+    let url = 'http://travelplanner.mobiliteit.lu/restproxy/departureBoard?accessId=cdt&format=json&';
+    console.log('Getting stop schedule for stop ' + stopApiId);
+    return request(url + stopApiId).then((data) => {
+        return JSON.parse(data);
+    });
+}
+
+function getStopApiIdByCoord(res, stop) {
+    let startX = Number((stop.geometry.coordinates[0]).toFixed(6)) * 1000000;
+    let startY = Number((stop.geometry.coordinates[1]).toFixed(6)) * 1000000;
+
+    let url = 'http://travelplanner.mobiliteit.lu/hafas/query.exe/dot?performLocating=2&tpl=stop2csv&look_maxdist=1500&look_x=' + startX + '&look_y=' + startY + '&stationProxy=yes';
+
+    console.log('Getting Stop RealTime API Id for ' + stop.properties.name);
+    return request(url).then((data) => {
+        let stopsRet = data.split('\n');
+        let stopNames = stopsRet.map(function(stop) {
+            return stop.split('@')[1];
+        })
+        let realName = stop.properties.name.split('/ ')[1].split('<br>')[0];
+        let bestMatch = {input: "", pos: -1};
+        for (let pos in stopNames) {
+            if (stopNames[pos]) {
+                let match = stopNames[pos].match(realName);
+                if (match && match.input.length > bestMatch.input.length) {
+                    bestMatch.input = match.input;
+                    bestMatch.pos = pos;
+                }
             }
-            ret.stops.push(point);
         }
+        console.log('Stop RealTime API Id found: ' + stopsRet[bestMatch.pos]);
+        return stopsRet[bestMatch.pos];
+    });
+}
+
+function getStopsCoordsByIds(data, stopIds) {
+    let stops = [];
+    for (let stopId of stopIds) {
+            for (let line of data) {
+                if (line.geometry.type == 'Point') {
+                    if (line.geometry.id == stopId) {
+                        stops.push(line);
+                    }
+                }
+            }
     }
-    return ret;
+    return stops;
 }
 
 function formatBusLineDataForGoogleAPI(data) {
