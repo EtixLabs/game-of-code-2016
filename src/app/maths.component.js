c70f185ad9fa20517a91a870283391c7ee24684e
Thomas Lacroix <thomas.lacroix@etixgroup.com>
2016-04-15 19:46:54 +0200

    (Maths)[Feat]: Add real bus lines

--- src/app/maths.component.js
+++ src/app/maths.component.js
@@ -5,18 +5,22 @@ import _ from 'lodash';
 
 class Controller {
     // @ngInject
-    constructor() {
+    constructor($http) {
         this.lines = [];
-        for (let i = 0; i < 35; ++i) {
-          this.lines.push((Math.random() * 500) | 0);
-        }
-        this.lines = _.uniq(this.lines);
-
+        $http.get('http://localhost:3000/bus').then((data) => {
+            this.lines = _(data.data)
+                .map(line => ({
+                    number: (line.name.match(/Ligne\-[0-9a-zA-Z]+/) || [null])[0].replace('Ligne-', ''),
+                    id: line.id
+                }))
+                .each(line => line.number = _.isNaN(+line.number) ? line.number : +line.number);
+            this.lines = _.orderBy(this.lines, 'number');
+        });
         this.selectedLine = null;
     }
 
     selectLine(line) {
-        this.selectedLine = line;
+        this.selectedLine = line.number;
     }
 }
 
